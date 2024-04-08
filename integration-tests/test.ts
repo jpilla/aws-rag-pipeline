32b0b76d738e2d349703d7e84cf3c6ae3bf509dc
Jonathan Pilla <jon.pilla@gmail.com>
2024-04-08 15:14:02 +0300

    super awesome stage, automated startup and integration tests, lots of room for improvement, but good start

---
+++ integration-tests/test.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+// Setting the base URL for axios makes it easier to make requests
+const api = axios.create({
+  baseURL: 'http://app:3000',
+});
+
+describe('GET', () => {
+  it('should return 200 and the expected message', async () => {
+    const response = await api.get('');
+    expect(response.status).toBe(200);
+    expect(response.data).toHaveProperty('message');
+    expect(response.data.message).toBe('hello world!');
+  });
+});
